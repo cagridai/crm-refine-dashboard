5f267db7bf1bb7c677b312af2e95aea419a13c12
Çağrı Dai <dai.cagri@gmail.com>
2024-01-25 09:53:31 +0300

    first commit

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,45 @@
-export { Header } from "./header";
+import UpcomingEvents from "@/components/home/upcoming-events";
+import DealsChart from "@/components/home/deals-chart";
+import UpcomingEventsSkeleton from "@/components/skeleton/upcoming-events";
+import AccordionHeaderSkeleton from "@/components/skeleton/accordion-header";
+import KanbanColumnSkeleton from "@/components/skeleton/kanban";
+import ProjectCardSkeleton from "@/components/skeleton/project-card";
+import LatestActivitiesSkeleton from "@/components/skeleton/latest-activities";
+import DashboardTotalCountCard from "@/components/home/total-count-card";
+import LatestActivities from "@/components/home/latest-activities";
+import { Accordion } from "@/components/accordion";
+import {
+  DescriptionHeader,
+  DueDateHeader,
+  UsersHeader,
+} from "@/components/tasks/form/header";
+import { DescriptionForm } from "@/components/tasks/form/description";
+import { DueDateForm } from "@/components/tasks/form/due-date";
+import { StageForm } from "@/components/tasks/form/stage";
+import { TitleForm } from "@/components/tasks/form/title";
+import { UsersForm } from "@/components/tasks/form/users";
+import { UserTag } from "@/components/tags/user-tags";
+import { Text } from "@/components/text";
+
+export {
+  UpcomingEvents,
+  DealsChart,
+  UpcomingEventsSkeleton,
+  AccordionHeaderSkeleton,
+  KanbanColumnSkeleton,
+  ProjectCardSkeleton,
+  LatestActivitiesSkeleton,
+  DashboardTotalCountCard,
+  LatestActivities,
+  Accordion,
+  DescriptionForm,
+  DescriptionHeader,
+  DueDateForm,
+  DueDateHeader,
+  StageForm,
+  TitleForm,
+  UsersForm,
+  UsersHeader,
+  UserTag,
+  Text,
+};
